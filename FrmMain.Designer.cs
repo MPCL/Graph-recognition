54a68418accefeab87df874dcfa00361af0de8ee
MPCL <00masoudpp@gmail.com>
2017-08-13 13:40:08 +0430

    Added main files

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Graphical
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDeg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblresualt = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Diagnosis Simple Graph ";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            // 
+            // txtDeg
+            // 
+            this.txtDeg.Location = new System.Drawing.Point(76, 34);
+            this.txtDeg.Name = "txtDeg";
+            this.txtDeg.Size = new System.Drawing.Size(299, 20);
+            this.txtDeg.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Degrees: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblresualt
+            // 
+            this.lblresualt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblresualt.Location = new System.Drawing.Point(155, 60);
+            this.lblresualt.Name = "lblresualt";
+            this.lblresualt.Size = new System.Drawing.Size(220, 23);
+            this.lblresualt.TabIndex = 4;
+            this.lblresualt.Text = ".";
+            this.lblresualt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(375, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 22);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(231, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 9);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Coded By: Masoud Pourghaffar (MPCL5)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(404, 100);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblresualt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDeg);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Graphical ";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDeg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblresualt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Date: 2017/8/13 
+ * Name : Graphical Finder
+ * Auther : Masoud Pourghaffar (MPCL5)
+ */ 
+
+namespace Graphical
+{
+    public partial class FrmMain : Form
+    {
+        // some codes for movale form
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void say_false()
+        {
+            lblresualt.BackColor = Color.Red;
+            lblresualt.Text = "Not graphical";
+        }
+
+        private void say_true()
+        {
+            lblresualt.BackColor = Color.LimeGreen;
+            lblresualt.Text = "Is graphical";
+        }
+
+        private bool check(string ctext)
+        {
+            // found from there: https://stackoverflow.com/questions/1818611/how-to-check-if-a-particular-character-exists-within-a-character-array
+            if (ctext.IndexOf('-') != -1)
+                return true;
+            else
+                return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            if (check(txtDeg.Text))
+                say_false();
+            else
+            {
+                string sorteddeg = Sort(',' + txtDeg.Text, ',');
+                while (true)
+                {
+                    var M = sorteddeg.Split(',');
+                    if (M[1] == "0")
+                    {
+                        say_true();
+                        break;
+                    }
+                    sorteddeg = "";
+                    for(int i=2;i<=(int.Parse(M[1])+1);i++)
+                    {
+                        try
+                        {
+                            sorteddeg = sorteddeg + ',' + (int.Parse(M[i]) - 1).ToString();
+                        }
+                        catch
+                        {
+                            say_false();
+                            return;
+                        }
+                    }
+                    if (check(sorteddeg))
+                    {
+                        say_false();
+                        break;
+                    }
+                    sorteddeg=Sort(sorteddeg,',');
+                    //MessageBox.Show(sorteddeg);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //for exit programe
+            Environment.Exit(1);
+        }
+
+        // A void for sort numbers
+        private string Sort(string Deg,char ch)
+        {
+            string result="";
+
+            for (int i = 1; ; i++)
+            {
+                try
+                {
+                    int lol = int.Parse(maximum(Deg, ch));
+                        for(int n=1;n<=count(Deg,lol.ToString(),lol.ToString().Length);n++)
+                        {
+                            result = result +ch+ maximum(Deg, ch);
+                        }
+                    Deg=Deg.Replace(','+maximum(Deg, ch),"");
+
+                }
+                catch{break; }
+            }
+            return result;
+        }
+
+        // A void for count any thing
+        private int count(string X,string l,int cc)
+        {
+            int L=0;
+            for(int i=0;i<X.Length;i++)
+            {
+                try
+                {
+                    string y = X.Substring(i, cc);
+                    if (y == l)
+                        L++;
+                }
+                catch { break; }
+            }
+            return L;           
+        }
+
+        // A void for return maximum
+        private string maximum(string X, char ch)
+        {
+            var slice = X.Split(ch);
+            int max = int.Parse(slice[1]);
+            for (int i = 2; i <= count(X, ch.ToString(), 1); i++)
+            {
+                if (int.Parse(slice[i]) > max)
+                {
+                    MessageBox.Show(slice[i]);
+                    max = int.Parse(slice[i]);
+                }
+            }
+            return max.ToString();
+        }
+
+        //make form moveable
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
